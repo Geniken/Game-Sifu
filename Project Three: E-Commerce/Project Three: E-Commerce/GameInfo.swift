5398d844c07763e1887bcb3a99b9455d8970c0f7
Daniel Kim <Msdk5500@gmail.com>
2016-10-03 11:26:51 -0700

    10/3

---
+++ Project Three: E-Commerce/Project Three: E-Commerce/GameInfo.swift
@@ -0,0 +1,20 @@
+//
+//  Info.swift
+//  Project Three: E-Commerce
+//
+//  Created by Daniel Kim on 9/30/16.
+//  Copyright © 2016 Daniel Kim. All rights reserved.
+//
+
+import Foundation
+
+struct GameInfo {
+    let image:URL
+    let ratings: Double
+    let reviews: [String]
+    let similarGames: [URL]
+    let description: String
+    let cost: Double
+}
+
+
