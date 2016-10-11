f60ee19960c217b2e2faa5ae63d86af2c08d062d
Daniel Kim <Msdk5500@gmail.com>
2016-10-11 16:58:08 -0700

    Oct 11, 2016 5 pm

--- Project Three/Project Three/LoginViewController.swift
+++ Project Three/Project Three/LoginViewController.swift
@@ -1,4 +1,3 @@
-//
 //  LoginViewController.swift
 //  Project Three
 //
@@ -13,25 +12,81 @@ import Parse
 class LoginViewController: UIViewController, UIWebViewDelegate {
     
     
+    @IBOutlet weak var userNameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBOutlet weak var loginButton: UIButton!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        query()
         
-//        let testObject = PFObject(className: "Name")
-//        testObject["name"] = "Daniel"
-//        testObject.saveInBackground { (success, error) -> Void in
-//            
-//            testObject.saveInBackground()
-//            print("Object has been saved.")
+        
+        print("current User: \(PFUser.current())")
+        
+        if let user = PFUser.current() {
+            print("USER: \(user)")
+            self.performSegue(withIdentifier: "signIn", sender: self)
+            
+            //        let testObject = PFObject(className: "Name")
+            //        testObject["name"] = "Daniel"
+            //        testObject.saveInBackground { (success, error) -> Void in
+            //
+            //            testObject.saveInBackground()
+            //            print("Object has been saved.")
+            
         }
         
     }
     
-//    override func didReceiveMemoryWarning() {
-//        super.didReceiveMemoryWarning()
-//  
-//    }
-//    
-//}
-
+    
+    var currentUser = PFUser.current()
+    
+    @IBAction func loginButtonPressed(_ sender: UIButton) {
+        
+        guard let userNameField = userNameTextField.text else { return }
+        guard let password = passwordTextField.text else { return }
+        
+        
+        
+        PFUser.logInWithUsername(inBackground: userNameField, password: password) { (user, error) in
+            
+            print(error)
+            print(user)
+            
+            if error != nil {
+                
+                self.performSegue(withIdentifier: "signIn", sender: self)
+                print("Yay you logged in")
+                print(user)
+                
+            } else {
+                
+                print ("error: \(error)")
+                
+            }
+        }
+    }
+    
+    func query() {
+        let query = PFUser.query()
+        query?.findObjectsInBackground(block: { (objects, error) in
+            
+            guard error == nil else { print(error) ; return }
+            guard let objects = objects else { return }
+            
+            for object in objects {
+                print(object)
+            }
+            
+        })
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        
+    }
+    
+}
