f08e760de306b28df2d93c776060eab91c7c31c0
kevintbradbury <kevin.t.bradbury@gmail.com>
2016-10-11 15:20:12 -0700

    Login to Heroku complete

--- Project Three/Project Three/LoginViewController.swift
+++ Project Three/Project Three/LoginViewController.swift
@@ -12,9 +12,16 @@ import Parse
 
 class LoginViewController: UIViewController, UIWebViewDelegate {
     
+    @IBOutlet weak var userNameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBOutlet weak var loginButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        PFUser.logOut()
         
         let testObject = PFObject(className: "Name")
         testObject["name"] = "Daniel"
@@ -24,13 +31,47 @@ class LoginViewController: UIViewController, UIWebViewDelegate {
             print("Object has been saved.")
         }
         
+        if currentUser != nil {
+            print(currentUser)
+            PFUser.getCurrentUserInBackground()
+        }else {
+            return
+        }
+        
     }
     
+    var currentUser = PFUser.current()
     
+    @IBAction func loginButtonPressed(_ sender: UIButton) {
+        
+        guard let userNameField = userNameTextField.text else { return }
+        guard let password = passwordTextField.text else { return }
+        
+        PFUser.logInWithUsername(inBackground: userNameField, password: password) { (user, error) in
+            
+            if error != nil {
+                
+                self.performSegue(withIdentifier: "signIn", sender: self)
+                print("Yay you logged in")
+                
+                
+            } else {
+                
+                print (error)
+                
+            }
+        }
+    }
+    
+    //    if currentUser != nil {
+    //    // Do stuff with the user
+    //    } else {
+    //    // Show the signup or login screen
+    //    }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-  
+        
     }
     
 }
