ad1ba626866e70957848093137dde4b63955d413
Daniel Kim <Msdk5500@gmail.com>
2016-10-14 13:23:59 -0700

    10.14 beta 1.3

--- Project Three/Project Three/CheckoutTableViewController.swift
+++ Project Three/Project Three/CheckoutTableViewController.swift
@@ -22,53 +22,41 @@ class CheckoutTableViewController: UITableViewController {
     
     var selectedIndex: Int?
     
-    var gameCheckoutInfoArray: [GameInfo] = []
-    
     override func numberOfSections(in tableView: UITableView) -> Int {
         return 1
         
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return gameCheckoutInfoArray.count
+        return checkOutCartArray.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "gameCheckoutCell", for: indexPath) as! CheckoutTableViewCell
         
-        let parseData = self.gameCheckoutInfoArray[indexPath.row]
+        let gameInfo = checkOutCartArray[indexPath.row]
         
-        cell.checkoutName.text = textNow
-        cell.checkoutImage.image = imageNow
-        cell.checkoutPrice.text = costNow
-        cell.checkoutConsole.text = consoleNow
+        cell.checkoutName.text = gameInfo.name
+        cell.checkoutImage.image = gameInfo.image
+        cell.checkoutPrice.text = gameInfo.cost
+        cell.checkoutConsole.text = gameInfo.console
         
         
         return cell
     }
     
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        
+        if editingStyle == .delete {
+            
+            checkOutCartArray.remove(at: indexPath.row)
+            
+            tableView.deleteRows(at: [indexPath], with: .fade)
+            
+        } else if editingStyle == .insert {
+          
+        }
+    }
     
-    //    override func tableView(_ tableView: UITableView, didSelectItemAt indexPath: IndexPath) {
-    //
-    //        let row = indexPath.row
-    //
-    //        guard row >= 0 && row < gameInfoArray.count else { return }
-    //
-    //        imageCheckout = gameInfoArray[row].image
-    //        nameCheckout = gameInfoArray[row].name
-    //        consoleCheckout = gameInfoArray[row].console
-    //        costCheckout = gameInfoArray[row].cost
-    //
-    //
-    //        self.performSegue(withIdentifier: "toCheckout", sender: self)
-    //    }
-    
-    
-    //    checkoutImage.image = imageCheckout
-    //    checkoutName.text = nameCheckout
-    //    checkoutConsole.text = consoleCheckout
-    //    checkoutPrice.text = costCheckout
-    
-    //}
 }
--- Project Three/Project Three/ManageItemsTableViewController.swift
+++ Project Three/Project Three/ManageItemsTableViewController.swift
@@ -9,16 +9,56 @@
 import Foundation
 import UIKit
 
-var manageItem:UIImage?
-var managedName:String?
-var manageCost:String?
-var manageConsole:String?
-
 
 class ManageItemsTableViewController: UITableViewController {
     
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
+        
     }
     
+    var selectedIndex:Int?
+    
+    override func numberOfSections(in tableView:UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView:UITableView, numberOfRowsInSection section:Int) -> Int {
+        return merchantItemsArray.count
+        
+    }
+    
+    override func tableView(_ tableView:UITableView, cellForRowAt indexPath:IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "manageItems", for: indexPath) as! MerchantItemTableViewCell
+        
+        let merchantGameInfo = merchantItemsArray[indexPath.row]
+        
+        cell.merchantItemImage.image = merchantGameInfo.image
+        
+        cell.merchantItemName.text = merchantGameInfo.name
+        
+        cell.merchantItemPrice.text = merchantGameInfo.cost
+        
+        cell.merchantItemConsole.text = merchantGameInfo.console
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        
+        if editingStyle == .delete {
+            
+            merchantItemsArray.remove(at: indexPath.row)
+            
+            tableView.deleteRows(at: [indexPath], with: .fade)
+            
+        } else if editingStyle == .insert {
+            
+        }
+    }
 }
